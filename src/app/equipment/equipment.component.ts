65856256da6bf22967862c7002d78064f55835b3
annettesharon <annettesharon@ymail.com>
2022-08-04 22:19:29 -0500

    First commit

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: object[] = [
+    {name: "Habitat dome"},
+    {name: "Drones"},
+    {name: "Food containers"},
+    {name: "Oxygen tanks"}
+  ];
+
+  equipmentBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(memberName: string) {
+
+    this.equipment.push({name: memberName});
+  }
+
+  remove(member: object) {
+    let index = this.equipment.indexOf(member);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.equipmentBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.equipmentBeingEdited = null;
   }
 
 }
